5b91eb7355e7ba5417468225c014ef38dc88987c
lautaro valle lopez <lautarovallelopez@gmail.com>
2020-12-16 15:05:00 -0300

    feat(home): add list group to home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 import {
     Col,
     Container,
     Row,
-    Nav,
-    NavItem,
-    NavLink
+    ListGroup,
+    ListGroupItem
 } from 'reactstrap';
+import {Link} from 'react-router-dom';
 import PackageJson from '@root/package.json';
 
-const Home = () => (
-    <Container>
-       <Row>
-            <Col sm={12} className="text-center">
-                <h1 className="oswald title-home">
-                    &nbsp;Bienvenido al Sistema&nbsp;
-                </h1>
-            </Col>
-        </Row>
-    </Container>
-);
+const Home = () => {
+    return (
+        <Container>
+            <Row>
+                <Col sm={{size:6, offset:3}} className="text-center">
+                    <h3>BIENVENIDO AL SISTEMA SICI</h3>
+                    <ListGroup>
+                        <ListGroupItem tag={'button', Link} to='/operativo' action >CODIFICACIÓN</ListGroupItem>
+                        <ListGroupItem tag="button" action>FUENTES OPERATIVOS</ListGroupItem>
+                        <ListGroupItem tag="button" action>VARABLES ESTADISTICAS</ListGroupItem>
+                        <ListGroupItem tag="button" action>NOMENCLATURAS</ListGroupItem>
+                        <ListGroupItem tag="button" action>DOCUMENTOS</ListGroupItem>
+                        <ListGroupItem tag="button" action>CHAT INTERNO</ListGroupItem>
+                        <ListGroupItem tag="button" action>MANEJO DE USUARIOS</ListGroupItem>
+                        {false && <ListGroupItem disabled tag="button" action>Vestibulum at eros</ListGroupItem>}
+                    </ListGroup>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+    
 
 export default Home;
